0ee88d060b31884365962ad5f0f2a9cb31d2b9cf
Nathan Hakkakzadeh <nate.hak@gmail.com>
2016-07-21 13:20:58 -0700

    Made code less cramped in reviewboard.py (increased line character length)

--- reviewboard.py
+++ reviewboard.py
@@ -6,16 +6,13 @@ import requests
 
 def get_review_request_from_id(id: int) -> dict:
     """Returns the decoded JSON payload for any id."""
-    resp = requests.get(
-                'http://reviewboard.mozilla.org/api/review-requests/{}/'
-                .format(id))
+    resp = requests.get('http://reviewboard.mozilla.org/api/review-requests/{}/'.format(id))
     return resp.json()
 
 def get_reviewers_from_id(id: int) -> str:
     try:
         resp = get_review_request_from_id(id)
-        return [person['title'] for person in
-                resp['review_request']['target_people']]
+        return [person['title'] for person in resp['review_request']['target_people']]
     except KeyError:
         return None
 
@@ -26,8 +23,7 @@ def get_summary_from_id(id: int) -> str:
     except KeyError:
         return ''
 
-def build_review_request_url(review_board_url: str,
-        review_request_id: int) -> str:
+def build_review_request_url(review_board_url: str, review_request_id: int) -> str:
     """Build a review request url based on the review board url and 
     review request id. Needed so we can infer the review request's url from the
     message.
