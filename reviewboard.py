411579d76db67ff752034d9f918352daba9127d4
Nathan Hakkakzadeh <nate.hak@gmail.com>
2016-07-21 13:41:47 -0700

    All code that touches any HTTP request is now asyncronous

--- reviewboard.py
+++ reviewboard.py
@@ -2,23 +2,26 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from irc3 import asyncio
 import requests
 
-def get_review_request_from_id(id: int) -> dict:
+async def get_review_request_from_id(id: int) -> dict:
     """Returns the decoded JSON payload for any id."""
-    resp = requests.get('http://reviewboard.mozilla.org/api/review-requests/{}/'.format(id))
+    loop = asyncio.get_event_loop()
+    fut = loop.run_in_executor(None, requests.get, 'http://reviewboard.mozilla.org/api/review-requests/{}/'.format(id))
+    resp = await fut
     return resp.json()
 
-def get_reviewers_from_id(id: int) -> str:
+async def get_reviewers_from_id(id: int) -> str:
     try:
-        resp = get_review_request_from_id(id)
+        resp = await get_review_request_from_id(id)
         return [person['title'] for person in resp['review_request']['target_people']]
     except KeyError:
         return None
 
-def get_summary_from_id(id: int) -> str:
+async def get_summary_from_id(id: int) -> str:
     try:
-        resp = get_review_request_from_id(id)
+        resp = await get_review_request_from_id(id)
         return resp['review_request']['summary']
     except KeyError:
         return ''
