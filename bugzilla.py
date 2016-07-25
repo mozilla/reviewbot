5401bc5427876668ac9066dca3eb2a20bc2869cd
Nathan Hakkakzadeh <nate.hak@gmail.com>
2016-07-24 19:13:52 -0700

    Added bugzilla.py which has a function to get a product:component for a bug id

---
+++ bugzilla.py
@@ -0,0 +1,10 @@
+from irc3 import asyncio
+import requests
+
+async def get_bugzilla_component(bug_id: str) -> str:
+    """Return the bugzilla product:component for the bug id."""
+    loop = asyncio.get_event_loop()
+    fut = loop.run_in_executor(None, requests.get, 'https://bugzilla.mozilla.org/rest/bug/{}'.format(bug_id))
+    resp = await fut
+    bug = resp.json()['bugs'][0]
+    return '{}:{}'.format(bug['product'], bug['component'])
